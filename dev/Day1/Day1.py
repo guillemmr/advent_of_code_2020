6fda5336a50e4cbf6980ded6cf288478b0d19889
guillemmr <guillemmr@hotmail.com>
2020-12-01 14:07:03 +0100

    Day1 Part 2

--- dev/Day1/Day1.py
+++ dev/Day1/Day1.py
@@ -2,21 +2,37 @@
 
 from itertools import combinations
 import os
+import numpy
 
 
-def product_of_nums_that_sums_2020(numbers: list) -> int:
-    for a, b in combinations(numbers, 2):
-        if a + b == 2020:
-            return a*b
+def product_nums_that_sums_2020(list_numbers: list, group_length: int) -> int:
+    """Find those numbers that adds to 2020 and return their product.
+
+    Args:
+        list_numbers (list): candidates to find which ones adds 2020
+        group_length (int): length of the searching group.
+            For example, if = 1, check if there's a single candidate 2020.
+                         if = 2,  search two candidates that adds 2020.
+
+    Raises:
+        Exception: No solution.
+
+    Returns:
+        int: Product of numbers that sums 2020
+    """
+
+    for current_candidates in combinations(list_numbers, group_length):
+        if numpy.sum(current_candidates) == 2020:
+            return numpy.prod(current_candidates)
     raise Exception("No solution")
 
 
-def read_input_and_solve() -> int:
+def read_input_and_solve(group_length: int) -> int:
     with open(os.path.join(os.path.dirname(__file__), "input.txt")) as file:
         numbers = [int(x) for x in file.read().splitlines()]
 
-    return product_of_nums_that_sums_2020(numbers)
+    return product_nums_that_sums_2020(numbers, group_length)
 
 
 if __name__ == "__main__":
-    print(read_input_and_solve())
+    print(read_input_and_solve(2))
--- test/Day1.py
+++ test/Day1.py
@@ -4,15 +4,22 @@ from dev.Day1 import Day1
 
 def test_exception():
     with pytest.raises(Exception, match=r".*No solution*"):
-        Day1.product_of_nums_that_sums_2020([1, 2, 3])
+        Day1.product_nums_that_sums_2020([1, 2, 3], 2)
 
 
 def test_solution_example():
     numbers = [1721, 979, 366, 299, 675, 1456]
-    result = Day1.product_of_nums_that_sums_2020(numbers)
+    result = Day1.product_nums_that_sums_2020(numbers, 2)
     assert result == 514579
 
 
 def test_solution1():
-    result = Day1.read_input_and_solve()
+    result = Day1.read_input_and_solve(2)
     assert result == 996075
+
+
+def test_solution2():
+    """Reduced verison from original"""
+    numbers = [1721, 979, 366, 299, 675, 1456]
+    result = Day1.product_nums_that_sums_2020(numbers, 3)
+    assert result == 241861950
