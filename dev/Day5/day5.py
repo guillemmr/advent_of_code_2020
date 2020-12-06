c5c9e38ee3ea6fb14f2453cf268dcf3261394781
guillemmr <guillemmr@hotmail.com>
2020-12-06 21:01:20 +0100

    Day5 Second_Part

--- dev/Day5/day5.py
+++ dev/Day5/day5.py
@@ -15,8 +15,24 @@ def get_seat_id(seat_code: str) -> int:
     return get_seat_row(seat_code)*8+get_seat_column(seat_code)
 
 
+def maximum_seat(seats_id: list):
+    return max(seats_id)
+
+
+def get_missing_seat(seats_id: list) -> int:
+    seats_id.sort()
+    space_between_seats = [y - x for x, y in zip(seats_id[:-1], seats_id[1:])]
+
+    index_of_missing_seat_neighbour = space_between_seats.index(2)
+    neighbour_id = seats_id[index_of_missing_seat_neighbour]
+
+    return neighbour_id + 1
+
+
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), "input.txt")) as file:
-        seats_id = file.readlines()
+        raw_seats = file.readlines()
 
-    print(max([get_seat_id(x) for x in seats_id]))
+    all_seats_id = [get_seat_id(x) for x in raw_seats]
+    print(maximum_seat(all_seats_id))
+    print(get_missing_seat(all_seats_id))
--- test/day5.py
+++ test/day5.py
@@ -20,3 +20,11 @@ def test_first_part_3():
     assert 102 == day5.get_seat_row(seat_code)
     assert 4 == day5.get_seat_column(seat_code)
     assert 820 == day5.get_seat_id(seat_code)
+
+
+def test_second_part():
+    assert 4 == day5.get_missing_seat([1, 2, 3, 5, 6])
+
+
+def test_missing_seat_always_returns_the_first_seat():
+    assert 4 == day5.get_missing_seat([1, 2, 3, 5, 7])
