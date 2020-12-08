de8373efa0052a5cbc2fe8b2bba5d5ef9c53e20b
guillemmr <guillemmr@hotmail.com>
2020-12-08 12:14:27 +0100

    Day8 Part 2

--- test/day8.py
+++ test/day8.py
@@ -1,7 +1,6 @@
 from dev.Day8 import day8
 
 
-
 def test_part1():
     instructions = """nop +0
 acc +1
@@ -13,4 +12,21 @@ acc +1
 jmp -4
 acc +6"""
     instructions = day8.read_intructions(instructions)
-    assert 5 == day8.get_accumulator_before_infinite_loop(instructions)
+    accumulator, eof = day8.execute_instructions(instructions)
+    assert accumulator == 5 and eof is False
+
+
+def test_part2():
+    instructions = """nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6"""
+    instructions = day8.read_intructions(instructions)
+    instructions = day8.fix(instructions)
+    accumulator, eof = day8.execute_instructions(instructions)
+    assert accumulator == 8 and eof is True
