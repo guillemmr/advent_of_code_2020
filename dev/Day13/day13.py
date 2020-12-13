2b8aeeecfb0e50b9ffaaaa6dbf03ae7a600c728e
guillemmr <guillemmr@hotmail.com>
2020-12-13 23:13:28 +0100

    Day13 part 2

--- dev/Day13/day13.py
+++ dev/Day13/day13.py
@@ -2,14 +2,63 @@ import os
 
 
 def get_id_bus_and_remaining_minutes(depart, buses):
-    valid_buses = [int(bus) for bus in buses if bus != "x" ]
-    remaining_minutes = [-(depart % bus -bus) for bus in valid_buses]
+    valid_buses = [int(bus) for bus in buses if bus != "x"]
+    remaining_minutes = [-((depart % bus)-bus) for bus in valid_buses]
     best_minutes = min(remaining_minutes)
     idx_best_bus = remaining_minutes.index(best_minutes)
 
     return (valid_buses[idx_best_bus], best_minutes)
-    
-        
+
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+
+def solve_congruence(a, b, n) -> set:
+    """Returns the pair (A,B) such the sequence {Ax+B}_x_in_Z is the solution of
+      ax = b mod (n)"""
+    a = a % n
+    b = b % n
+    a_inverse = modinv(a, n)
+    return(n, b*a_inverse % n)
+
+
+def first_consecutive_depart(buses: list) -> int:
+    """ returns a timestamp t such that:
+        t = 0 mod buses[0]
+        t+1 = 0 mod buses[1]
+        ...
+        t+n = 0 mod buses[n]
+        doesn't include equations where buses[i] is the character "x"
+    """
+    # The solutions have the form ax+b
+    a = 1
+    b = 0
+    for i, bus in enumerate(buses):
+        if bus == "x":
+            continue
+        bus = int(bus)
+
+        # Solve the equation a*x + b = -i mod bus
+        new_a, new_b = solve_congruence(a, -i-b, bus)
+        # Ok, x have the form {new_a*n + new_b}_n
+        # General sol: a(new_a*n+new_b)+b = (a*new_a)*n +(a*new_b + b) =:a'n+b'
+        b = a*new_b + b
+        a = a*new_a
+    return b % a
+
 
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), "input.txt")) as file:
@@ -18,4 +67,4 @@ if __name__ == "__main__":
     buses = buses.split(",")
     bus_id, minute = get_id_bus_and_remaining_minutes(depart, buses)
     print(bus_id * minute)
-
+    print(first_consecutive_depart(buses))
--- test/day13.py
+++ test/day13.py
@@ -1,5 +1,6 @@
 from dev.Day13 import day13
 
+
 def test_example1():
     depart, buses = """939
 7,13,x,x,59,x,31,19""".splitlines()
@@ -8,4 +9,16 @@ def test_example1():
 
     bus_id, minute = day13.get_id_bus_and_remaining_minutes(depart, buses)
     assert 295 == bus_id * minute
-test_example1()
+
+
+def test_example2():
+    buses = "17,x,13,19".split(",")
+    assert 3417 == day13.first_consecutive_depart(buses)
+    buses = "67,7,59,61".split(",")
+    assert 754018 == day13.first_consecutive_depart(buses)
+    buses = "67,x,7,59,61".split(",")
+    assert 779210 == day13.first_consecutive_depart(buses)
+    buses = "67,7,x,59,61".split(",")
+    assert 1261476 == day13.first_consecutive_depart(buses)
+    buses = "1789,37,47,1889".split(",")
+    assert 1202161486 == day13.first_consecutive_depart(buses)
