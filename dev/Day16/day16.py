52194c2804647e849de2bd4603a3f84b729d8b3c
guillemmr <guillemmr@hotmail.com>
2020-12-20 15:46:07 +0100

    Day16 Part 2

--- dev/Day16/day16.py
+++ dev/Day16/day16.py
@@ -16,6 +16,12 @@ def satisfy_some_rule(rules, x) -> bool:
     return False
 
 
+def all_values_valids(values: list, expr) -> bool:
+    if all(list(map(lambda val: satisfy_rule(expr, val), values))):
+        return True
+    return False
+
+
 def get_rules_and_tickets(raw_rules_and_tickets):
     main_parts = raw_rules_and_tickets.split("\n\n")
     raw_rules, raw_tikets, raw_nearby_tickets = main_parts
@@ -48,6 +54,69 @@ def calc_scanning_error_rate(rules: dict, nearby_tickets: list) -> int:
     return ser
 
 
+def discard_invalid_tickets(rules: dict, nearby_tickets: list) -> list:
+    valid_tickets = nearby_tickets.copy()
+    for ticket in nearby_tickets:
+        for value in ticket:
+            if not satisfy_some_rule(rules, value):
+                valid_tickets.remove(ticket)
+    return valid_tickets
+
+
+def get_unique_field(col: list, fields_to_assign: dict):
+    """ Returns the unique field name that satisfies all the values.
+        Returns None if more than one field can be assigned.
+    """
+
+    unique_field = None
+
+    for field_name, rule in fields_to_assign.items():
+        if all_values_valids(col, rule):
+
+            if unique_field is not None:
+                return None
+
+            unique_field = field_name
+
+    if unique_field is None:
+        raise Exception("Found a column where no field can be assigned!")
+
+    return unique_field
+
+
+def figure_out_fields(fields: dict, valid_tickets: list) -> dict:
+    """ Returns {field_name -> idx of the ticket}.
+
+        Alg: search iteratively for those unasigned ticket idx that a unique
+            field can be assigned
+    """
+
+    position_fields = {}
+
+    cols_to_assign = list(range(0, len(valid_tickets[0])))
+    fields_to_assign = fields.copy()
+
+    while len(cols_to_assign) != 0:
+
+        for col_idx in cols_to_assign:
+            col = [ticket[col_idx] for ticket in valid_tickets]
+
+            unique_field = get_unique_field(col, fields_to_assign)
+
+            if unique_field is not None:
+                unique_matching_column = col_idx
+                break
+
+        if unique_field is None:
+            raise Exception("Infinite recursion.")
+
+        cols_to_assign.remove(unique_matching_column)
+        fields_to_assign.pop(unique_field)
+        position_fields[unique_field] = unique_matching_column
+
+    return position_fields
+
+
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), "input.txt")) as file:
         raw_rules_and_tickets = file.read()
@@ -56,3 +125,16 @@ if __name__ == "__main__":
     rules, my_ticket, nearby_tickets = info
 
     print(calc_scanning_error_rate(rules, nearby_tickets))
+
+    valid_tickets = discard_invalid_tickets(rules, nearby_tickets)
+    position_fields = figure_out_fields(rules, valid_tickets)
+
+    departure_idxs = [idx
+                      for field, idx in position_fields.items()
+                      if field.startswith("departure")
+                      ]
+
+    total_prod = 1
+    for idx in departure_idxs:
+        total_prod *= my_ticket[idx]
+    print(total_prod)
--- test/day16.py
+++ test/day16.py
@@ -17,3 +17,29 @@ nearby tickets:
     info = day16.get_rules_and_tickets(tickets_info)
     rules, my_ticket, nearby_tickets = info
     assert 71 == day16.calc_scanning_error_rate(rules, nearby_tickets)
+
+
+def test2():
+    tickets_info = """class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9
+"""
+    info = day16.get_rules_and_tickets(tickets_info)
+    rules, my_ticket, nearby_tickets = info
+
+    valid_tickets = day16.discard_invalid_tickets(rules, nearby_tickets)
+    position_fields = day16.figure_out_fields(rules, valid_tickets)
+
+    total_prod = 1
+    for idx, field_name in enumerate(position_fields.keys()):
+        total_prod *= my_ticket[idx]
+
+    assert 1716 == total_prod
