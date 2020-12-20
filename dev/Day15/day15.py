021ad724000c465a5305d1218979cfd86b025485
guillemmr <guillemmr@hotmail.com>
2020-12-20 12:29:28 +0100

    Day15 Part 2

--- dev/Day15/day15.py
+++ dev/Day15/day15.py
@@ -1,41 +1,50 @@
 import os
+import line_profiler
+
+profile = line_profiler.LineProfiler()
 
 
+@profile
 def play_one_turn(starting_numbers: list):
 
     for num in starting_numbers[:-1]:
         yield num
 
-    numbers = starting_numbers.copy()
     last_position = {
         val: idx + 1
         for (idx, val) in enumerate(starting_numbers[:-1])
         }
 
-    while True:
-        current_value = numbers[-1]
+    next_num = starting_numbers[-1]
+    current_idx = len(starting_numbers)-1
+    del starting_numbers
 
-        if current_value in last_position:
-            current_idx = len(numbers)
-            next_num = current_idx - last_position[current_value]
-        else:
+    while 1:
+        current_value = next_num
+        current_idx += 1
+
+        try:
+            next_num = -last_position[current_value] + current_idx
+        except KeyError:
             next_num = 0
 
-        last_position[current_value] = len(numbers)
+        last_position[current_value] = current_idx
 
-        numbers.append(next_num)
         yield current_value
 
 
+@profile
 def play(starting_numbers: list, num_turns: int) -> int:
     """Returns the number that will be played at turn @num_turns"""
-    for turn, value in enumerate(play_one_turn(starting_numbers)):
-        if turn == num_turns-1:
-            return value
+    number_played_generator = play_one_turn(starting_numbers)
+    for x in range(num_turns-1):
+        next(number_played_generator)
+    return next(number_played_generator)
 
 
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), "input.txt")) as file:
         starting_numbers = list(map(int, file.read().split(",")))
-
     print(play(starting_numbers=starting_numbers, num_turns=2020))
+    # num_turns = 30000000
+    # print(play(starting_numbers=starting_numbers, num_turns=num_turns))
